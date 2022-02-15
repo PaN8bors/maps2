2cf48142a2363594e635a8dbeaf0e8a6c519e74d
Parish Neighbors <pn8bors@gmail.com>
2022-02-15 10:48:38 -0600

    Second commit

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ import input from 'input';
 
 async function prompt(grades) {
     while (true) {
-    let name = await input.text('Enter student name or "cancel"');
+      let name = await input.text('Enter student name or "cancel"');
       if (name === "cancel") {
           break;
       } else {
           let grde = await input.text('What is their grade?')
-    grades.set(name, grde)
+      grades.set(name, grde)
       }
     }
 }
 
 function display(grades) {
   for (const [name, grde] of grades) {
-      console.log(`${name} has a ${grde}`);
+      console.log(`${name} has a ${grde}%`);
   }
 }
 
