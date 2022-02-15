bab6a1439affcb86cce3ad48a564b45dafadbaa9
Parish Neighbors <pn8bors@gmail.com>
2022-02-15 10:43:11 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,28 @@
+import input from 'input';
+
+async function prompt(grades) {
+    while (true) {
+    let name = await input.text('Enter student name or "cancel"');
+      if (name === "cancel") {
+          break;
+      } else {
+          let grde = await input.text('What is their grade?')
+    grades.set(name, grde)
+      }
+    }
+}
+
+function display(grades) {
+  for (const [name, grde] of grades) {
+      console.log(`${name} has a ${grde}`);
+  }
+}
+
+async function main() {
+    const grades = new Map();
+
+    await prompt(grades);
+    display(grades);
+}
+
+main();
